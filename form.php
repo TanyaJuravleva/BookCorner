9a4d61be52dc6182c447829fc3c8844f48395202
TanyaJuravleva <krilltt@mail.ru>
2023-11-06 04:34:01 +0300

    добавление формы

---
+++ form.php
@@ -0,0 +1,16 @@
+<div id="modal-1" class="modal micromodal-slide" aria-hidden="false">
+    <div data-custom-close="1" tabindex="-1" class="modal__overlay">
+        <div aria-labelledby="modal-1-title" role="dialog" aria-modal="true" class="modal__container">
+            <header class="modal__header"><h2 id="modal-1-title" class="modal__title">Modal 1</h2>
+                <a arial-label="Close modal" data-custom-close="1" href="#" class="close-btn w-button">ｘ</a>
+             </header>
+            <main id="modal-1-content" class="modal__content">
+                <p>Content of Modal 1<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
+            </main><div class="modal__footer">
+            <a arial-label="Close this dialog window" data-custom-close="1" href="#" class="modal__close-btn w-inline-block">
+                <div>Close this modal</div>
+            </a>
+          </div>
+        </div>
+    </div>
+</div>
