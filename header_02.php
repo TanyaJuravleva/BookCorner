9c6559de11dcd00fca2302056db15be272802b3e
TanyaJuravleva <krilltt@mail.ru>
2023-12-14 04:19:48 +0300

    попытка писать на bootstrap, переписана админка

---
+++ header_02.php
@@ -0,0 +1,27 @@
+<header class="container-fluid main-header">
+    <div class="container cont-top">
+        <div class="row">
+            <h1 class="main-header__title row"><a href="#">BOOKCORNER</a></h1>
+            <div class="main-header__user">
+                <button type="button" class="btn btn-outline-secondary main-header__enter-btn">Войти</button>
+                <button type="button" class="btn btn-link main-header__reg-btn">Регистрация</button>
+            </div>
+        </div>
+        <div class="row">
+            <nav class="navbar navbar-expand-lg col-8">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav main-header__list">
+                        <li class="nav-item main-header__list_item"><a class="main-header__list_item-href" href="#">Главное</a></li>
+                        <li class="nav-item main-header__list_item"><a class="main-header__list_item-href" href="#">Жанры</a></li>
+                        <li class="nav-item main-header__list_item"><a class="main-header__list_item-href" href="#">Популярное</a></li>
+                        <li class="nav-item main-header__list_item"><a class="main-header__list_item-href" href="#">Авторы</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+</header>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
---
+++ login_form.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная</title>
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/header_02.css">
+    <link rel="stylesheet" href="./css/login_form.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <?php 
+    include "./header_02.php";
+    ?>
+    <div class="text-center">
+    
+    <main class="form-signin">
+      <form>
+        <!-- <img class="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> -->
+        <h1 class="h3 mb-3 fw-normal">Вход</h1>
+    
+        <div class="form-floating">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+          <label for="floatingInput">Почта</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+          <label for="floatingPassword">Пароль</label>
+        </div>
+    
+        <div class="checkbox mb-3">
+          <!-- <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label> -->
+        </div>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Войти</button>
+        <!-- <p class="mt-5 mb-3 text-muted">© 2017–2021</p> -->
+      </form>
+    </main>
+</div>
+  </body>
+</html>
